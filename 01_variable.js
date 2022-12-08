98b5e6a6decc8eb18231ac06eb1cd94eede77b2a
kanakareeta <kanakareeta@gmail.com>
2022-12-08 18:55:58 +0530

    Java123:modified file
    Java123:modified file

---
+++ 01_variable.js
@@ -0,0 +1,27 @@
+var myName;//Varible Declaration
+myName="Reeta Kanaka";//variable initialization
+console.log(myName)
+
+var myFrdName="Varsha Shelake";//Variable declaration and initialization at the same time
+
+console.log(myFrdName);
+myFrdName=" Kamal Mirkal";//// Variable declaration and initialization at the same time
+
+console.log(myFrdName);//
+
+myFullName="Reeta Shrikant Kanaka";
+console.log(myFullName);//
+
+var myLove;
+myLove="I Love Javascript"
+console.log(myLove);//
+
+var myName="Reeta Kanaka"
+var Age="31"
+var address="Sangola"
+var pincode="41309"
+var gender="Female"
+var married="Yes"
+console.log(myName,Age,address,pincode,gender,married);
+
+         
